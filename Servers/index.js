014861e553bf6c6e169bdaf7ae2e739726a82c04
[nirmal] <nirmallamrin77@gmail.com>
2024-12-05 10:49:58 +0530

    cors updated

--- Servers/index.js
+++ Servers/index.js
@@ -15,7 +15,14 @@ const Port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+
+  })
+);
+
 
 
 app.use("/user", userRouter)
